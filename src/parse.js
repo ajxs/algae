f8e3e842b9b56c0ac9eaae7f302f4423a6949b1a
Anthony JXS <ajxscc@gmail.com>
2017-08-13 00:09:12 +1000

    Code quality improvements

--- src/parse.js
+++ src/parse.js
@@ -1,81 +1,13 @@
 "use strict";
 
-const $_ALGAE_REGEX_VAR = /\#\!\$([^\<\>\s]*)/;
+const $_ALGAE_REGEX_VAR = /\#\!\$(.*)/;
 const $_ALGAE_REGEX_THIS = /\#\!\%/;
 const $_ALGAE_REGEX_EXPR = /\#\!\^\((.*)\)/;
 
-$_algae.parseDomElement = (currentNode, data = {}, parentData = data) => {
-	// Elements need to exist on the DOM prior to parsing
-	let parentNode = currentNode.parentNode;
-	/*
-		Note: on an element with a loop directive, any display directive belongs to the
-		individual elements in the loop, and their corresponding data
-	*/
-	if(currentNode.dataset.loopSource) {
-		let loopSourceArray = null;
-		try {
-			loopSourceArray = $_algae.parseExpression(currentNode.dataset.loopSource, data, parentData);
-		} catch(e) {
-			console.error(e);
-			loopSourceArray = null;
-		}
-
-		if(!loopSourceArray) {
-			console.warn(`loop-source iterable "${currentNode.dataset.loopSource}" evaluates as 'null'.\nIgnoring.`);
-			loopSourceArray = [];    // break out
-		}
-
-		let loopContainer = document.createDocumentFragment();
-		loopSourceArray.forEach(source => {
-			// create a clone of the 'template node', from which we will create the looped element.
-			let newTemplateItem = currentNode.cloneNode(true);
-
-			// remove the loop source data from the generated children.
-			delete newTemplateItem.dataset.loopSource;
-			loopContainer.appendChild(newTemplateItem);
-
-			// newly generated children are parsed independently with their own context
-			// from the loop source array. - we parse this here so we can control it's context
-			$_algae.parseDomElement(newTemplateItem, source, parentData);
-		});
-
-		// replace 'loop template' with parsed loop items
-		parentNode.insertBefore(loopContainer, currentNode);
-		parentNode.removeChild(currentNode);
-
-		// child elements have already been parsed with their own data context during creation
-		return;
-	}
-
-	if(currentNode.dataset.displayCondition) {
-		try {
-			if(!$_algae.parseExpression(currentNode.dataset.displayCondition, data, parentData)) {
-				// Remove the child if the expression evaluates as false
-				// no need to parse the element here, it will be parsed anyway
-				parentNode.removeChild(currentNode);
-			}
-			delete currentNode.dataset.displayCondition;
-		} catch(e) {
-			console.error(e);
-			return null;
-		}
-	}
-
-	// parse attributes
-	Array.from(currentNode.attributes).forEach(a => a.value = $_algae.parseText(a.value, data, parentData));
-	// parse inner text
-	currentNode.firstChild.nodeValue = $_algae.parseText(currentNode.firstChild.nodeValue || "", data, parentData);
-	// Recursive call at end to avoid accidentally clobbering child scope with parent in scoped directives
-	Array.from(currentNode.children).forEach(i => $_algae.parseDomElement(i, data, parentData));
-};
-
-
 /*
-This function is only to be used for parsing certain attributes,
-which are predetermined to return data and not strings.
-@TODO: document this properly.
+Function for parsing algae attribute strings, returns the data referenced within
 */
-$_algae.parseExpression = (text = "", data = {}, parentData = {}) => {
+$_algae.parseAttributeString = (text = "", data = {}, parentData = {}) => {
 	if(!text) return null;
 
 	let test = null;
@@ -129,4 +61,69 @@ $_algae.parseText = (text = "", data = {}, parentData = {}) => {
 		});
 
 		return ret;
+};
+
+
+/*
+Function for parsing each DOM element within a template.
+Elements need to exist on the DOM prior to parsing
+*/
+$_algae.parseDomElement = (currentNode, data = {}, parentData = data) => {
+	let parentNode = currentNode.parentNode;
+
+	/* Note: on an element with a loop directive, any display directive belongs to the
+		individual elements in the loop, and their corresponding data */
+	if(currentNode.dataset.loopSource) {
+		let loopSourceArray = null;
+		try {
+			loopSourceArray = $_algae.parseAttributeString(currentNode.dataset.loopSource, data, parentData);
+		} catch(e) {
+			console.error(e);
+			loopSourceArray = null;
+		}
+
+		if(!loopSourceArray) {
+			console.warn(`loop-source iterable "${currentNode.dataset.loopSource}" evaluates as 'null'.\nIgnoring.`);
+			loopSourceArray = [];    // break out
+		}
+
+		let loopContainer = document.createDocumentFragment();
+		loopSourceArray.forEach(source => {
+			// create a clone of the 'template node', from which we will create the looped element.
+			let newTemplateItem = currentNode.cloneNode(true);
+
+			delete newTemplateItem.dataset.loopSource;    // avoid bad recursion
+			loopContainer.appendChild(newTemplateItem);
+
+			// newly generated children are parsed independently with their own context
+			// from the loop source array. - we parse this here so we can control it's context
+			$_algae.parseDomElement(newTemplateItem, source, parentData);
+		});
+
+		// replace 'loop template' with parsed loop items
+		parentNode.replaceChild(loopContainer, currentNode);
+
+		return;    // child elements have already been parsed with their own data context
+	}
+
+	if(currentNode.dataset.displayCondition) {
+		try {
+			if(!$_algae.parseAttributeString(currentNode.dataset.displayCondition, data, parentData)) {
+				// Remove the child if the expression evaluates as false
+				// no need to parse the element here, it will be parsed anyway
+				parentNode.removeChild(currentNode);
+			}
+			delete currentNode.dataset.displayCondition;
+		} catch(e) {
+			console.error(e);
+			return null;
+		}
+	}
+
+	// parse attributes
+	Array.from(currentNode.attributes).forEach(a => a.value = $_algae.parseText(a.value, data, parentData));
+	// parse inner text
+	currentNode.firstChild.nodeValue = $_algae.parseText(currentNode.firstChild.nodeValue || "", data, parentData);
+	// Recursive call at end to avoid accidentally clobbering child scope with parent in scoped directives
+	Array.from(currentNode.children).forEach(i => $_algae.parseDomElement(i, data, parentData));
 };
