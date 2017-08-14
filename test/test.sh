cfd1eec251b1bc9487101b2d10110b2cc965852d
Anthony JXS <ajxscc@gmail.com>
2017-08-14 22:10:07 +1000

    Testing improvements

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # search for all files named output_.txt to base our tests on
-TEST_FILES=$(ls output*.txt 2>/dev/null)
+TEST_FILES=$(ls test*.html 2>/dev/null)
 if [[ -z "${TEST_FILES}" ]]
 	then
 		printf "No test files found!\nExiting...\n"
@@ -10,23 +10,24 @@ fi
 
 for TEST_FILE in ${TEST_FILES}
 do
-	TEST_NUMBER=$(echo ${TEST_FILE} | sed "s/output\(.*\).txt/\1/")
+	#TEST_NUMBER=$(echo ${TEST_FILE} | sed "s/output\(.*\).txt/\1/")
+	RAW_NAME=$(echo ${TEST_FILE} | sed "s/\(.*\).html/\1/")
 
 	# Check for the corresponding
-	TEMPLATE_FILE="test${TEST_NUMBER}.html"
-	if [[ ! -f "${TEMPLATE_FILE}" ]]
+	OUTPUT_FILE="${RAW_NAME}_output.txt"
+	if [[ ! -f "${OUTPUT_FILE}" ]]
 		then
-			printf "Corresponding template file to ${TEST_FILE} not found!\nExiting...\n"
+			printf "Corresponding output file to ${TEST_FILE} not found!\nExiting...\n"
 			exit 1
 	fi
 
-	DOM_OUTPUT=$(google-chrome --headless --disable-gpu --dump-dom file://${PWD}/${TEMPLATE_FILE});
-	EXPECTED_OUTPUT=$(cat ${TEST_FILE})
+	DOM_OUTPUT=$(google-chrome --headless --disable-gpu --dump-dom file://${PWD}/${TEST_FILE});
+	EXPECTED_OUTPUT=$(cat ${OUTPUT_FILE})
 
 	if [[ "${DOM_OUTPUT}" == "${EXPECTED_OUTPUT}" ]]
-		then printf "Test ${TEST_NUMBER}: PASSED.\n"
+		then printf "Test ${TEST_FILE}: PASSED.\n"
 		else
-			printf "Test ${TEST_NUMBER}: FAILED.\nTests unsuccessful.\nExiting...\n"
+			printf "Test ${TEST_FILE}: FAILED.\nTests unsuccessful.\nExiting...\n"
 			exit 1
 	fi
 done
